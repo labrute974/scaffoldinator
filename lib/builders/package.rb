1be0ab76f9f07ad39fb2943b7c1edb3580b042f3
Karel Malbroukou Moendzenahou <karel.malbroukou@gmail.com>
2016-02-12 01:00:42 +1100

    build template for package task and run script on host

--- lib/builders/package.rb
+++ lib/builders/package.rb
@@ -1,9 +1,55 @@
 ###############################################
 # Build shell script part for the packages task
 
+require 'erb'
+
+def get_template()
+  %{
+##########################
+# PACKAGE <%= @pkg_name %>
+
+pkg="<%= @pkg_name %>"
+dryrun="<%= @dry_run %>"
+os=$(sed '1!d' /etc/issue)
+
+## Check if package installed
+function check {
+  case $os in
+    *Ubuntu*)
+      cmd="dpkg -l $pkg"
+      ;;
+  esac
+
+  $cmd > /dev/null >&2
+  return $?
+}
+
+## Install the package
+function install {
+  case $os in
+    *Ubuntu*)
+      cmd="apt-get -y install $pkg"
+      ;;
+  esac
+
+  [[ "$dryrun" == "true" ]] && echo "dry run: ${cmd}" || $cmd
+}
+
+check && echo " - Package $pkg is already installed." || install
+
+##### END PACKAGE <%= @pkg_name %> #####
+  }
+end
+
+
 module Builders
-  class Packages
-    def self.build
+  class Package
+    def self.build pkg, dry_run = false
+      @pkg_name = pkg["name"]
+      @dry_run  = dry_run
+
+      renderer = ERB.new(get_template)
+      renderer.result(binding)
     end
   end
 end
