72e2791f022848c758a95563374e51aafe42c49f
Karel Malbroukou Moendzenahou <karel.malbroukou@gmail.com>
2016-02-11 23:40:56 +1100

    package builder module
    builders are used to build the shell script part of the specific task

---
+++ lib/builders.rb
@@ -0,0 +1,4 @@
+module Builders
+end
+
+require "builders/package"
---
+++ lib/builders/package.rb
@@ -0,0 +1,9 @@
+###############################################
+# Build shell script part for the packages task
+
+module Builders
+  class Packages
+    def self.build
+    end
+  end
+end
