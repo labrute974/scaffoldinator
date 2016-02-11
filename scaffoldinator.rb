5ec9e088f8b3d5a0444e0d84bfa8e96db038b17e
Karel Malbroukou Moendzenahou <karel.malbroukou@gmail.com>
2016-02-11 22:16:21 +1100

    read taskfile

--- scaffoldinator.rb
+++ scaffoldinator.rb
@@ -1,14 +1,25 @@
 #! /usr/bin/env ruby
 
 require "thor"
+require "yaml"
+
+def err(output)
+  error "Error: #{output}"
+  exit 1
+end
 
 class Scaffoldinator < Thor
   desc "planner", "Build the script that runs on the local/remote server"
-  option :taskfile, :aliases => "-t", :desc => "specify a tasks file"
+  option :taskfile, :aliases => "-t", :type => :string, :desc => "specify a tasks file"
   option :"dry-run", :aliases => "-d", :type => :boolean, :desc => "See what the tasks is going to do"
   option :verbose, :aliases => "-v", :type => :boolean, :desc => "Verbose mode."
   def planner
-    puts options.inspect
+    # Test if task file passed exists
+    err("File #{options[:taskfile]} does not exist.") unless File.exists? options[:taskfile]
+
+    yaml = YAML.load_file options[:taskfile]
+
+    puts yaml.inspect
   end
 end
 
